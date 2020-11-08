1f27e0a9b95b675d481bc5129fd729f1a7e7a13c
”denysyatsenko228” <dayyz@bk.ru>
2020-11-08 21:58:20 +0200

    connected html with Django

--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -1,21 +1,28 @@
-"""studentsdb URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import patterns, include, url
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+urlpatterns = patterns('',
+        #Students urls
+        url(r'^$', 'students.views.students_list', name='home'),
+        url(r'^students/add/$', 'students.views.students_add',
+        	name='students_add'),
+        url(r'^students/(?P<sid>\d+)/edit/$',
+        	'students.views.students_edit',
+        	name='students_edit'),
+        url(r'^students/(?P<sid>\d+)/delete/$',
+        	'students.views.students_delete',
+        	name='students_delete'),
+        
+        #Groups urls
+        url(r'^groups/$', 'students.views.groups_list', name='groups'),
+        url(r'^groups/add/$', 'students.views.groups_add',
+        	name='groups_add'),
+        url(r'^groups/(?P<gid>\d+)/edit/$',
+        	'students.views.groups_edit',
+        	name='groups_edit'),
+        url(r'^groups/(?P<gid>\d+)/delete/$',
+        	'students.views.groups_delete',
+        	name='groups_delete'),
+        	
+        url(r'^admin/', include(admin.site.urls)),
+)
